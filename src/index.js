7c29d37b06e8940ddb474e3445dcf2f0daba5712
Ivan <fortross59@gmail.com>
2019-10-29 22:10:03 +0500

    update console.log

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ export const playGame = (gameCondition, getRoundData) => {
     if (userAnswer === roundCorrectAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${roundCorrectAnswer}'.\nLet's try again, ${nameUser}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${roundCorrectAnswer}'.`);
+      console.log(`Let's try again, ${nameUser}!`);
       return;
     }
   }
